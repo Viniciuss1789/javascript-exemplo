592fc0334086aee69cee10effebd6f8364b8199a
Viniciuss1789 <viniciussebastiao592@gmail.com>
2025-10-06 20:08:57 -0300

    feat: Initial commit of the To-Do List project

---
+++ js/scritpt10.js
@@ -0,0 +1,18 @@
+let numero = 6
+alert("O valor da variável número é : " + numero)
+let numero01 = 7
+alert("O valor da variável número é : " + numero01)
+numero = numero01
+alert("O valor da variável número é : " + numero)
+
+//-----------------------------------------------------//
+
+numero = 6
+alert("O valor da variável número é : " + numero)
+numero01 = 7
+alert("O valor da variável número é : " + numero01)
+numero += numero01
+alert("O valor da variável número é : " + numero)
+
+//-----------------------------------------------------//
+
---
+++ js/scritpt11.js
@@ -0,0 +1,7 @@
+let soma = "10"
+
+if(typeof soma === "string"){
+    alert("Número igual a 10")
+}else{
+    alert("Número não igual a 10")
+}
