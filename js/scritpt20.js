79fc6599534d0ac3866ecbbf814035d2759077b5
Viniciuss1789 <viniciussebastiao592@gmail.com>
2025-10-30 19:52:02 -0300

    feat: Initial commit of the To-Do List projeto

---
+++ js/scritpt20.js
@@ -0,0 +1,10 @@
+let hearder = "Template Strings";
+let tags = ["templates strings", "javascript", "es6"]
+
+let html = `<h2>${hearder}</h2><ul>`;
+for (const x of tags){
+    html += `<li>${x}</li>`;
+}
+
+html += `</ul>`;
+document.getElementById("demo").innerHTML = html;
